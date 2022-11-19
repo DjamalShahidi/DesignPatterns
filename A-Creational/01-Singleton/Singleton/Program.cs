8e76e3dbfef3ffe51e2d42b2815fd8695f02425a
jamal shahidi <jamalshahidi20@gmail.com>
2022-11-19 14:45:46 +0330

    Add some info for singleton pattern

--- A-Creational/01-Singleton/Singleton/Program.cs
+++ A-Creational/01-Singleton/Singleton/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//The intent of the Singleton pattern is to ensure that a class
+//1-Only has one instance 
+//2-Provide a global point of access to it 
+//----------
+//Real-life example:
+//1-Logger
+//----------
+//Note
+//1-Prefer lazy instantiation
+
+
+
 Console.WriteLine("Hello, World!");
