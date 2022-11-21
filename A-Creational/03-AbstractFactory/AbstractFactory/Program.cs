49e0ee036e277dfe15fefb2847711df71344c530
jamal shahidi <jamalshahidi20@gmail.com>
2022-11-21 14:54:39 +0330

    Init AbstractFactory and write the intent of it

---
+++ A-Creational/03-AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,4 @@
+﻿//The intent of the abstract factory pattern is to provide an interface for 
+//careating families of related or dependent objects without specifying 
+//their concrete classes
+Console.WriteLine("Hello, World!");
