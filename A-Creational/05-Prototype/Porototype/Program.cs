9d1a054e303bc56fad8c3219801601988691f6d1
jamal shahidi <jamalshahidi20@gmail.com>
2022-11-22 16:45:56 +0330

    Implement Prototype

---
+++ A-Creational/05-Prototype/Porototype/Program.cs
@@ -0,0 +1,19 @@
+﻿using Prototype;
+// The intent of this pattern is to specify the kinds of objects to create 
+// using a prototypical instance,and create new objects by copying this prototype
+Console.Title = "Portotype";
+
+var manager = new Manager("Cindy");
+var managerClone = (Manager)manager.Clone();
+Console.WriteLine($"Manager was cloned :{managerClone.Name}");
+
+var employee = new Employee("Kevin", managerClone);
+var employeeClone = (Employee)employee.Clone(true);
+Console.WriteLine($"Employee was cloned :{employeeClone.Name}, with manager {employeeClone.Manager.Name}");
+
+managerClone.Name = "Karen";
+Console.WriteLine($"Employee was cloned :{employeeClone.Name}, with manager {employeeClone.Manager.Name}");
+
+
+
+Console.ReadKey();
