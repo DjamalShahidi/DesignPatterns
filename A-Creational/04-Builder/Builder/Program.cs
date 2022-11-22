a11c26ccf3f59df9ba5803c735c1bdfd153328b8
jamal shahidi <jamalshahidi20@gmail.com>
2022-11-22 15:45:19 +0330

    Implement Builder

---
+++ A-Creational/04-Builder/Builder/Program.cs
@@ -0,0 +1,23 @@
+﻿using Builder;
+// The intent of the builder pattern is to separate  the construction of 
+// a complex object from its represention .By doing so, the same construction 
+// process can create different representations.
+
+Console.Title = "Builder";
+
+var garage = new Garage();
+
+var miniBuilder = new MiniBuilder();
+var bmwBuilder = new BMWBuilder();
+
+garage.Construct(miniBuilder);
+Console.WriteLine(miniBuilder.Car.ToString());
+//or:
+garage.Show();
+
+garage.Construct(bmwBuilder);
+Console.WriteLine(bmwBuilder.Car.ToString());
+//or:
+garage.Show();
+
+Console.ReadKey();
